4e24942036b264ae214a16d4a72afe215febb6f7
Ubuntu <ubuntu@ip-172-31-36-253.eu-west-1.compute.internal>
2020-05-11 21:34:33 +0000

    Add create and destroy sessions & nav login / logout links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in?
+  
+  def current_user
+    @current_user || User.find(session[:user_id]) if session[:user_id] #Checks if current user exists IF not checks the users table and returns user
+  end
+
+  def logged_in?
+    !!current_user #turns current user into boolean
+  end
+  
+  def require_user
+     if !logged_in?
+       flash[:alert] = "You must be logged in to perform that action"
+       redirect_to login_path
+     end
+  end
+  
 end
